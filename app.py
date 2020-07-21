b2e369de1df30f0ee15c400a1118c91876caeab7
SouravGupta143 <souravgupta354@gmail.com>
2020-07-22 01:37:31 +0530

    Adding News and Nifty

--- app.py
+++ app.py
@@ -41,68 +41,171 @@ def index():
 def topstock():
     if request.method=='POST':
         text=request.form['search']
-        db,cur=connect('tsa')
-        cur.execute('SELECT * FROM companies WHERE title==?',(text,))
-        url_text=cur.fetchone()
-        db.close()
-        url=url_text[2]
-        req= Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-        data= urlopen(req).read()
-        # return render_template('test.html',data=data)
-        soup=BeautifulSoup(data,'lxml')
-        li=soup.find_all('table',{'class':'table table-bordered table-striped table-hover'})
-        final=[]
-        for val in li:
-            final.append(val.find_all('td'))
-        #for var in val.find_all('td')
-        final1=[]
-        arr=np.array(final)
-        final=arr.take([0,4,8,9,10])
-        final1=arr.take([2,3])
-        final2=arr.take(12)
-        final=final.tolist()
-        final1=final1.tolist()
-        i=1
-        final_dict={}
-        for val in final:
-            lib=[]
-            if len(val)%2==0:
-                for value in val:
-                    lib.append(value.text.strip())
-                dic={lib[k]:lib[k+1] for k in range(0,len(lib),2)}
-                final_dict[i]=dic
-                i=i+1
-                
-            else:
-                lis=val[0:(len(val)-1)]
-                for value in lis:
-                    lib.append(value.text.strip())
-                dic={lib[k]:lib[k+1] for k in range(0,len(lib),2)}
-                final_dict[i]=dic
-                i=i+1
+        if text=='NIFTY - S&P CNX NIFTY - INDEX':
+            db,cur=connect('tsa')
+            cur.execute('SELECT * FROM companies WHERE title==?',(text,))
+            url_text=cur.fetchone()
+            db.close()
+            url=url_text[2]
+            req= Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+            data= urlopen(req).read()
+            # return render_template('test.html',data=data)
+            soup=BeautifulSoup(data,'lxml')
+            li=soup.find_all('div',{'class':'col-lg-4 col-md-4 col-sm-4'})[1:]
+            val_list=[]
+            final_dict={}
+            h3=li[0].find('h3').text
+            new_li=li[0].find('table').find_all('td')
+            for val in new_li:
+                val_list.append(val.text)
+            val_dic={val_list[i]:val_list[i+1] for i in range(0,len(val_list),2)}
+            final_dict[h3]= val_dic
 
-        i=1
-        final_dict1={}
-        for val in final1:
-            lib=[]
-            if len(val)%3==0:
-                for value in val:
-                    lib.append(value.text.strip())
-                dic1={lib[k]:lib[k+1:k+3] for k in range(0,len(lib),3)}
-                final_dict1[i]=dic1
+            for div in li[1:4]:
+                val_list=[]
+                h3=div.find('h3').text
+                new_li=div.find('table').find_all('td')
+                for val in new_li:
+                    val_dic={}
+                    if len(val.attrs)!=0 :
+                        if val.attrs['colspan']=='3':
+                            pass
+                        else:
+                            
+                            val_list.append(val.text.strip())
+                    else:
+                        val_list.append(val.text.strip())
+                val_dic={val_list[i]:val_list[i+1:i+3] for i in range(0,len(val_list),3)}
+                final_dict[h3]= val_dic
+
+            val_list=[]
+            h3=li[4].find('h3').text
+            new_li=li[4].find('table').find_all('td')
+            val_dic={}
+            for val in new_li:
+                if len(val.attrs)!=0 :
+                    if val.attrs['colspan']=='2':
+                        pass
+                    else:
+                        val_list.append(val.text.strip())
+                else:
+                    val_list.append(val.text.strip())
+            val_dic={val_list[i]:val_list[i+1] for i in range(0,len(val_list),2)}
+            final_dict[h3]= val_dic
+
+            for div in li[4:8]:
+                val_list=[]
+                h3=div.find('h3').text
+                new_li=div.find('table').find_all('td')
+                for val in new_li:
+                    val_dic={}
+                    if len(val.attrs)!=0 :
+                        if val.attrs['colspan']=='2':
+                            pass
+                        else:
+                            val_list.append(val.text.strip())
+                    else:
+                        val_list.append(val.text.strip())
+                val_dic={val_list[i]:val_list[i+1] for i in range(0,len(val_list),2)}
+                final_dict[h3]= val_dic
+
+            val_list=[]
+            h3=li[8].find('h3').text
+            new_li=li[8].find('table').find_all('td')
+            for val in new_li:
+                val_dic={}
+                if len(val.attrs)!=0 :
+                    if val.attrs['colspan']=='3':
+                        pass
+                    else:
+                        val_list.append(val.text.strip())
+                else:
+                    val_list.append(val.text.strip())
+            val_dic={val_list[i]:val_list[i+1:i+3] for i in range(0,len(val_list),3)}
+            final_dict[h3]= val_dic
+
+            li=soup.find('div',{'class':'col-lg-8 col-md-8 col-sm-8'})
+            val_list=[]
+            h3=li.find('h3').text.strip()
+            for val in li.find_all('td'):
+                if len(val.attrs)!=0 :
+                    if val.attrs['colspan']=='1':
+                        pass
+                    else:
+                        val_list.append(val.text.strip())
+                else:
+                    val_list.append(val.text.strip())
+            final_dict[h3]= val_list
+            final={}
+            i=1
+            for val in final_dict.items():
+                final[i]=val
                 i=i+1
-                
-            else:
-                lis1=val[0:(len(val)-1)]
-                for value in lis1:
-                    lib.append(value.text.strip())
-                dic1={lib[k]:lib[k+1:k+3] for k in range(0,len(lib),3)}
-                final_dict1[i]=dic1
-                i=i+1
-        highlights=[]
-        for val in final2:
-            highlights.append(val.text.strip())
-        return render_template('topstock.html',data1=final_dict, data2=final_dict1,highlight=highlights)
+   
+            return render_template('nifty.html',data=final)
+        else:
+            db,cur=connect('tsa')
+            cur.execute('SELECT * FROM companies WHERE title==?',(text,))
+            url_text=cur.fetchone()
+            db.close()
+            url=url_text[2]
+            req= Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+            data= urlopen(req).read()
+            # return render_template('test.html',data=data)
+            soup=BeautifulSoup(data,'lxml')
+            li=soup.find_all('table',{'class':'table table-bordered table-striped table-hover'})
+            final=[]
+            for val in li:
+                final.append(val.find_all('td'))
+            #for var in val.find_all('td')
+            final1=[]
+            arr=np.array(final)
+            final=arr.take([0,4,8,9,10])
+            final1=arr.take([2,3])
+            final2=arr.take(12)
+            final=final.tolist()
+            final1=final1.tolist()
+            i=1
+            final_dict={}
+            for val in final:
+                lib=[]
+                if len(val)%2==0:
+                    for value in val:
+                        lib.append(value.text.strip())
+                    dic={lib[k]:lib[k+1] for k in range(0,len(lib),2)}
+                    final_dict[i]=dic
+                    i=i+1
+                    
+                else:
+                    lis=val[0:(len(val)-1)]
+                    for value in lis:
+                        lib.append(value.text.strip())
+                    dic={lib[k]:lib[k+1] for k in range(0,len(lib),2)}
+                    final_dict[i]=dic
+                    i=i+1
+
+            i=1
+            final_dict1={}
+            for val in final1:
+                lib=[]
+                if len(val)%3==0:
+                    for value in val:
+                        lib.append(value.text.strip())
+                    dic1={lib[k]:lib[k+1:k+3] for k in range(0,len(lib),3)}
+                    final_dict1[i]=dic1
+                    i=i+1
+                    
+                else:
+                    lis1=val[0:(len(val)-1)]
+                    for value in lis1:
+                        lib.append(value.text.strip())
+                    dic1={lib[k]:lib[k+1:k+3] for k in range(0,len(lib),3)}
+                    final_dict1[i]=dic1
+                    i=i+1
+            highlights=[]
+            for val in final2:
+                highlights.append(val.text.strip())
+            return render_template('topstock.html',data1=final_dict, data2=final_dict1,highlight=highlights)
 
 @app.route('/fund_ac/', methods=['POST'])
 def screener():
@@ -234,39 +337,80 @@ def connect(dbname):
         exit(2)
 
 def fetchnews(dbname):
-    db,cur=connect(dbname)
-    cur.execute('SELECT * FROM News ORDER BY id DESC LIMIT 1')
-    lastrec=cur.fetchone()
-    newslist=[]
-    for var in range(1,5):
-        url='https://www.cnbctv18.com/market/stocks/page-{0}/'.format(var)
-        data=urlopen(url)
-        soup=BeautifulSoup(data,'lxml')
-        li=soup.find_all('div',{'class':'list_title'})
-        for div in li: 
-            link=div.find('a')['href']
-            text=list(div.find('a').text.split('\n'))
+    if dbname=='cnbctv18':
+        db,cur=connect('cnbctv18')
+        cur.execute('SELECT * FROM News ORDER BY id DESC LIMIT 1')
+        lastrec=cur.fetchone()
+        newslist=[]
+        for var in range(1,5):
+            url='https://www.cnbctv18.com/market/stocks/page-{0}/'.format(var)
+            data=urlopen(url)
+            soup=BeautifulSoup(data,'lxml')
+            li=soup.find_all('div',{'class':'list_title'})
+            for div in li: 
+                link=div.find('a')['href']
+                text=list(div.find('a').text.split('\n'))
+                if text[0]==lastrec[1]:
+                    break
+                score=vader.polarity_scores(text[0])['compound']
+                if score<0:
+                    senti='negative'
+                elif score==0:
+                    senti='netural'
+                else:
+                    senti='positive'
+                newslist.append((link,text,senti))
             if text[0]==lastrec[1]:
                 break
-            score=vader.polarity_scores(text[0])['compound']
-            if score<0:
-                senti='negative'
-            elif score==0:
-                senti='netural'
-            else:
-                senti='positive'
-            newslist.append((link,text,senti))
-        if text[0]==lastrec[1]:
-            break
-    if len(newslist)!=0:        
-        newslist=newslist[::-1]
+        if len(newslist)!=0:        
+            newslist=newslist[::-1]
 
-        for val in newslist:
-            cur.execute("""INSERT INTO News(title,description,link,sentiment)
-                            VALUES(?,?,?,?)""",(val[1][0],val[1][1],val[0],val[2]))
-            db.commit()
-    db.close()
-    return None
+            for val in newslist:
+                cur.execute("""INSERT INTO News(title,description,link,sentiment)
+                                VALUES(?,?,?,?)""",(val[1][0],val[1][1],val[0],val[2]))
+                db.commit()
+        db.close()
+        return None
+    
+    elif dbname=='moneycontrol':
+        db,cur=connect('moneycontrol')
+        cur.execute('SELECT * FROM News ORDER BY id DESC LIMIT 1')
+        lastrec=cur.fetchone()
+        newslist=[]
+        for var in range(1,3):
+            url='https://www.moneycontrol.com/news/business/stocks/page-{0}'.format(var)
+            data=urlopen(url)
+            soup=BeautifulSoup(data,'lxml')
+            li=soup.find_all('li',{'class':'clearfix'})
+            for div in li:
+                try:
+                    link=div.find('a')['href']
+                    title=div.find('a')['title']
+                    desc=div.find('p').text
+                    if title==lastrec[1]:
+                        break
+                    else:
+                        score=vader.polarity_scores(title)['compound']
+                        if score<0:
+                            senti='negative'
+                        elif score==0:
+                            senti='netural'
+                        else:
+                            senti='positive'
+                        newslist.append((title,desc,senti,link))
+                except:
+                    pass
+            if title==lastrec[1]:
+                break
+
+        if len(newslist)!=0:        
+                newslist=newslist[::-1]
+                for val in newslist:
+                    cur.execute("""INSERT INTO News(title,description,sentiment,link)
+                        VALUES(?,?,?,?)""",(val[0].strip(),val[1].strip(),val[2],val[3]))
+                    db.commit()
+        db.close()
+
 
 
 
@@ -277,10 +421,19 @@ def news():
     cur.execute("""SELECT * FROM (
     SELECT * FROM News ORDER BY id DESC LIMIT 5
         )Var1 ORDER BY id ASC;""")
+    news1=cur.fetchall()
+    news1=news1[::-1]
+    db.close()
+
+    fetchnews('moneyconrol')
+    db,cur=connect('moneycontrol')
+    cur.execute("""SELECT * FROM (
+    SELECT * FROM News ORDER BY id DESC LIMIT 5
+        )Var1 ORDER BY id ASC;""")
     news2=cur.fetchall()
     news2=news2[::-1]
     db.close()
-    return render_template('news.html',data=enumerate(news2,start=1))
+    return render_template('news.html',data1=enumerate(news1,start=1),data2=enumerate(news2,start=1))
 
 @app.route('/cnbctv/')
 def cnbctv18news():
@@ -306,17 +459,31 @@ def part_cnbc_news():
     db.close()
     return render_template("cnbc_newser.html",data=enumerate(data,start=1))
 
-# def dic(c_name):
-#     return ({"name":c_name[0]})
 
-# @app.route('/tsahome/')
-# def tsa_home():
-#     db,cur=connect('tsa')
-#     cur.execute('Select title from companies')
-#     data=cur.fetchall()
-#     db.close()
-#     c_list= [ dic(name) for name in data]
-#     return jsonify(c_list)
+@app.route('/money_news/')
+def moneynews():
+    db,cur=connect('moneycontrol')
+    cur.execute("SELECT * FROM News")
+    news=cur.fetchall()
+    news=news[::-1]
+    db.close()
+    return render_template('money_control.html',data=enumerate(news,start=1))
+
+
+@app.route('/part_money_news/', methods=['POST'])
+def part_money_news():
+    if request.method == 'POST':
+        word=request.form['search']
+        db,cur=connect('moneycontrol')
+        cur.execute('Select * from News')
+        news_list=cur.fetchall()
+        word=word.lower()
+        data=[]
+        for news in news_list:
+            if word in news[1].lower():
+                data.append(news)
+    db.close()
+    return render_template("money_search.html",data=enumerate(data,start=1))
 
 
 @app.route('/sgd/')
@@ -326,6 +493,9 @@ def receive_data():
 @app.route('/fundamentals/')
 def fund_data():
     return render_template('fundamentals.html')
+
+
+
 
 if __name__ == "__main__":  
     app.run(debug = True) 
